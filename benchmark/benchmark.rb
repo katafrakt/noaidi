6163b920796dd03602c5f078fdbc72c1f99db6e6
Pawel Swiatkowski <pawel.swiatkowski@fyber.com>
2016-05-29 22:18:52 +0200

    Better benchmark

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -7,9 +7,10 @@ module NoaidiBenchmark
   module_function
 
   def run
+    values = (1..n).map{ |i| i.odd? ? [:odd, i] : [:even, i] }
     Benchmark.bmbm do |x|
-      x.report("#{n} times without compile") { run_without_compile }
-      x.report("#{n} times with compile")    { run_with_compile }
+      x.report("#{n} times without compile") { run_without_compile(values) }
+      x.report("#{n} times with compile")    { run_with_compile(values) }
     end
   end
 
@@ -18,22 +19,24 @@ module NoaidiBenchmark
     n > 0 ? n : N
   end
 
-  def run_without_compile
-    (1..n).each do |val|
+  def run_without_compile(values)
+    values.each do |val|
       Noaidi.match(val) do |m|
-        m.(String) { raise StandardError('this should never be called') }
-        m.(Integer) { |i| i }
+        m.(:odd, String) { raise StandardError('this should never be called') }
+        m.(:odd, Integer) { |i| i }
+        m.(:even, Integer) { |i| i + 1 }
       end
     end
   end
 
-  def run_with_compile
+  def run_with_compile(values)
     match = Noaidi.compile_match do |m|
-      m.(String) { raise StandardError('this should never be called') }
-      m.(Integer) { |i| i }
+      m.(:odd, String) { raise StandardError('this should never be called') }
+      m.(:odd, Integer) { |i| i }
+      m.(:even, Integer) { |i| i + 1 }
     end
 
-    (1..n).each do |val|
+    values.each do |val|
       match.(val)
     end
   end
